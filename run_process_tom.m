9716f491c16121022b892daa08e845144d871d2a
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2014-11-18 21:17:03 +0000

    Save all

--- run_process_tom.m
+++ run_process_tom.m
@@ -4,3 +4,7 @@ rho2exp = state_tom(Xplus_norm_Xproc, Yplus_norm_Xproc, Zminus_norm_Xproc, Xplus
 rho3exp = state_tom(Xplus_norm_Xproc, Yplus_norm_Xproc, Zminus_norm_Xproc, Yminus_Xproc, 2.15e-5 , 2.15e-5, 8.15e-5, .7e-5)
 
 chi_exp = process_tom( rho1exp, rho4exp, rho2exp, rho3exp )
+
+plot_complex_matrix(chi_exp)
+
+fidelity(chi_exp, chi_X)
